22eea4bc568dfe0a96daea290d126b9391380a79
tec4006 <136395757+tec4006@users.noreply.github.com>
2023-06-13 12:12:37 +0000

    tec nerd

--- src/router/index.js
+++ src/router/index.js
@@ -16,10 +16,7 @@ const routes = [
       {
         path: '/Os',
         name: 'Os',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Os.vue'),
+        component: () => import('@/views/Os.vue'),
       },
       {
         path: '/Acionamento',
